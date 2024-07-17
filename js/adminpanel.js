c6824a87e9021a2c8758d76ad95c6a6a3a33ec83
Safal Kunwar <129306053+safalkunwar@users.noreply.github.com>
2024-07-17 21:49:31 +0545

    UI design added  ; not necessary

--- js/adminpanel.js
+++ js/adminpanel.js
@@ -1,4 +1,17 @@
+    const firebaseConfig = {
+            apiKey: "AIzaSyBZpFhPq1pFpvTmyndOnA6SRs9_ftb4jfI",
+        authDomain: "v-track-gu999.firebaseapp.com",
+        databaseURL: "https://v-track-gu999-default-rtdb.firebaseio.com",
+        projectId: "v-track-gu999",
+        storageBucket: "v-track-gu999.appspot.com",
+        messagingSenderId: "1046512747961",
+        appId: "1:1046512747961:web:80df40c48bca3159296268",
+        measurementId: "G-38X29VT1YT"
+        };
 
+        // Initialize Firebase
+        const app = firebase.initializeApp(firebaseConfig);
+        const db = firebase.database();
 function submitNotice() {
     var noticeContent = document.getElementById('noticeContent').value;
     if (noticeContent.trim() === '') {
